c67753ed9c14726a84504c2c5e2b7d7c6cb79a10
unknown <saidasfour20@gmail.com>
2020-12-02 20:40:25 -0800

    3

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ fetch('https://trainingsaid.herokuapp.com/')
   .then(json => {
     for(let i=0;i<json.length;i++){
 var x=document.createElement('div');
-var content=`<div class="col-sm-4 ">
+
+var content=`<div style="margin:5%">
+<div class="col-sm-4 ">
 <div class="card" style="width:400px">
 <img class="card-img-top" src="xxx" alt="Card image">
 <div class="card-body">
   <h4 class="card-title">yyy</h4>
   <a href="#" class="btn btn-primary">Watch movie</a>
   <a href="#" class="btn btn-dark" style="margin-left:20%">Download movie</a>
+</div>
 </div>
 </div>
 </div>`
