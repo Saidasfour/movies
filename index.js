81b7b5c0bed8c12b8dab4651c935c9e6aa1f9ec1
unknown <saidasfour20@gmail.com>
2020-12-02 18:18:36 -0800

    eightt

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ fetch('https://trainingsaid.herokuapp.com/')
   .then(json => {
     for(let i=0;i<json.length;i++){
 var x=document.createElement('div');
-var content=`<div class="col-lg-4 ">
-<div class="card" style="width:300px">
+var content=`<div class="col-sm-4 ">
+<div class="card" style="width:400px">
 <img class="card-img-top" src="xxx" alt="Card image">
 <div class="card-body">
   <h4 class="card-title">yyy</h4>
